a0ea9342a5ac3b6e51d66baf608e53648a892e2b
Mahedi Hasan <mahedi302@gmail.com>
2022-12-23 16:37:38 +0600

    fix end date issue in total report

--- src/services/productActivity/totalReports.ts
+++ src/services/productActivity/totalReports.ts
@@ -1,6 +1,7 @@
-import { getTokenInfo } from '~/lib/auth/authlib';
 import axios from 'axios';
+
 import { API_URL } from '~/environments';
+import { getTokenInfo } from '~/lib/auth/authlib';
 type ParamsType = {
   start_date: string;
   end_date: string;
@@ -13,7 +14,7 @@ export const getTotalReport = (params: ParamsType) => {
         .get(`${API_URL}/total-report/`, {
           params: {
             start_date: params?.start_date,
-            end_date: params?.start_date,
+            end_date: params?.end_date,
           },
           headers: {
             Authorization: `Bearer ${token}`,
