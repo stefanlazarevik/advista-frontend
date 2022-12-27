10c7871d3870b9b0e7308587b1613185b3055db0
alexkozminski <102160228+alexkozminski@users.noreply.github.com>
2022-12-27 18:07:06 +0600

    Fix the user table

--- src/components/UserTable/UserTable.tsx
+++ src/components/UserTable/UserTable.tsx
@@ -164,7 +164,7 @@ const UserTable = () => {
                       key={person.id}
                       className="odd:bg-gray-50 even:bg-white"
                     >
-                      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6 ">
+                      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm text-slate-700 sm:pl-6 ">
                         <div className="h-10 w-10 flex-shrink-0">
                           <img
                             className="h-10 w-10 rounded-full"
@@ -177,22 +177,22 @@ const UserTable = () => {
                           />
                         </div>
                       </td>
-                      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
+                      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm text-slate-700 sm:pl-6">
                         <div className="flex items-center">
                           <div>
-                            <div className="font-medium text-gray-900">
+                            <div className=" text-sm text-slate-700">
                               {`${person.first_name} ${person.last_name}`}
                             </div>
-                            <div className="text-gray-500">
+                            <div className="text-sm text-gray-500">
                               @{person.username}
                             </div>
                           </div>
                         </div>
                       </td>
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                      <td className="whitespace-nowrap px-3 py-4 text-sm text-slate-700">
                         {person.email}
                       </td>
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                      <td className="whitespace-nowrap  px-3 py-4 text-sm">
                         <Switch
                           checked={person.is_active}
                           onChange={() => {
