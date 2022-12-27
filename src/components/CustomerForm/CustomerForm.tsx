899ed28a3c9177ec80e59e159beeadc0a0fb1b8d
alexkozminski <102160228+alexkozminski@users.noreply.github.com>
2022-12-27 18:56:55 +0600

    Fix the images

--- src/components/CustomerForm/CustomerForm.tsx
+++ src/components/CustomerForm/CustomerForm.tsx
@@ -84,7 +84,7 @@ const CustomerForm = ({ mode }: Props) => {
         user_mode: Number(user_mode),
       });
       if (user_mode === '2') {
-        setUserMode(false);
+        setUserMode(true);
       }
     }
   }, [data, mode, reset]);
@@ -105,7 +105,7 @@ const CustomerForm = ({ mode }: Props) => {
     },
   );
   const onSubmit = (data: any) => {
-    data.avatar = data.avatar[0];
+    data.avatar = data.avatar !== null ? data.avatar[0] : null;
     console.log(data);
     if (mode === 'create') {
       toast.promise(
@@ -132,9 +132,7 @@ const CustomerForm = ({ mode }: Props) => {
     console.log('error', error);
   };
   useEffect(() => {
-    if (userMode) {
-      setValue('user_mode', userMode ? 2 : 1);
-    }
+    setValue('user_mode', userMode ? 2 : 1);
   }, [setValue, userMode]);
 
   return (
@@ -281,42 +279,43 @@ const CustomerForm = ({ mode }: Props) => {
                   Avatar
                 </label>
                 <div className="mt-1 sm:col-span-2 sm:mt-0">
-                  <div className="flex max-w-lg justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
+                  <div className="pb-6s flex max-w-lg justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5">
                     <div className="space-y-1 text-center">
-                      <svg
-                        className="mx-auto h-12 w-12 text-gray-400"
-                        stroke="currentColor"
-                        fill="none"
-                        viewBox="0 0 48 48"
-                        aria-hidden="true"
-                      >
-                        <path
-                          d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                          strokeWidth={2}
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
+                      {mode === 'edit' && data?.avatar_thumb !== null ? (
+                        <img
+                          src={`${API_BASE_URL}${data?.avatar_thumb}`}
+                          alt="profile pic"
                         />
-                      </svg>
-                      <div className="flex text-sm text-gray-600">
-                        {mode === 'edit' && data?.avatar_thumb !== null ? (
-                          <img
-                            src={`${API_BASE_URL}${data?.avatar_thumb}`}
-                            alt="profile pic"
+                      ) : (
+                        <svg
+                          className="mx-auto h-12 w-12 text-gray-400"
+                          stroke="currentColor"
+                          fill="none"
+                          viewBox="0 0 48 48"
+                          aria-hidden="true"
+                        >
+                          <path
+                            d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                            strokeWidth={2}
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
                           />
-                        ) : (
-                          <label
-                            htmlFor="file-upload"
-                            className="relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500"
-                          >
-                            <span>Upload a file</span>
-                            <input
-                              id="file-upload"
-                              type="file"
-                              className="sr-only"
-                              {...register('avatar')}
-                            />
-                          </label>
-                        )}
+                        </svg>
+                      )}
+
+                      <div className="flex text-sm text-gray-600">
+                        <label
+                          htmlFor="file-upload"
+                          className="relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500"
+                        >
+                          <span>Upload a file</span>
+                          <input
+                            id="file-upload"
+                            type="file"
+                            className="sr-only"
+                            {...register('avatar')}
+                          />
+                        </label>
                         <p className="pl-1">or drag and drop</p>
                       </div>
                       <p className="text-xs text-gray-500">
